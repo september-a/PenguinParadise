c42650c3f69efd5403229c132813d3d2a012f5de
september-a <september.abbott@gmail.com>
2023-03-25 18:31:45 -0500

    this one works

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ from tkinter import ttk
 
 def buildWindow():
     win = Tk()
+    frame = Frame(win)
     win.geometry("650x250")
 
     width = win.winfo_screenwidth()               
     height = win.winfo_screenheight()               
     win.geometry("%dx%d" % (width, height))
+    
+    frame.pack()
 
     label= Label(win, text= "Penguin Paradise", font=('Times New Roman bold',20))
     label.pack(padx=10, pady=10)
 
     p1 = PanedWindow()
-    p1.pack(fill=BOTH, expand=2)
+    p1.pack(fill=BOTH, expand=TRUE)
 
     left = Label(p1, text="Left Panel", background="yellow")
     p1.add(left)
-    p1.pack(padx=(width/5), pady=(height/5))
-
+    numberEntry = Entry(text = "This is an entry")
+    
+    p1.add(numberEntry)
+    
     p2 = PanedWindow(p1)
     p2.pack(fill=BOTH, expand=2)
     p1.add(p2)
 
     c1 = Canvas(p1, bg="blue")
+    
+    p1.pack()
+    
     c1.pack(fill=BOTH)
     p1.add(c1)
 
