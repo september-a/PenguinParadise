72505eb326c4687332fe734f3a1c02b4fcf9b0b8
Trevor Garnett <tjg0020@uah.edu>
2023-03-26 10:01:13 -0500

    randomly spawning penguins set to fixed number. TODO: make it so peguins alter birth and death rates and trash spawns in randomly and can be clicked away by user

--- canvas.py
+++ canvas.py
@@ -1,17 +1,17 @@
 import tkinter as tk
-from tkinter import *
 from PIL import Image,ImageTk
 import random
 
-def _imagePrep(text):
+def _image_prep(text):
         image = Image.open(text)
-        resized_image = image.resize((int(image.width/10),int(image.height/10)), Image.Resampling.LANCZOS)
+        resized_image = image.resize((int(image.width/10),int(image.height/10)), Image.ANTIALIAS)
         return ImageTk.PhotoImage(resized_image)
 
 class MyCanvas(tk.Tk):
     def __init__(self,root):
-        MyCanvas.penguinImage = _imagePrep("penguin.png")
-        MyCanvas.trashbagImage = _imagePrep("trashbag.png")
+        MyCanvas.redPenguin = _image_prep("redPenguin.png")
+        MyCanvas.penguinImage = _image_prep("penguin.png")
+        MyCanvas.trashbagImage = _image_prep("trashbag.png")
         self.canvas = tk.Canvas(root,background="lightblue")
         root.bind('<Configure>', self.resize)
     
@@ -19,13 +19,18 @@ class MyCanvas(tk.Tk):
         self.height = event.width
         self.width = event.height
 
-    def drawPenguin(self):
-        self.canvas.create_image(random.randint(5, 100),random.randint(5,100),anchor=NW,image=MyCanvas.penguinImage)
+    def drawPenguin(self,x,y):
+        penguin = ""
+        if(random.randint(1,2)==1):
+            penguin = MyCanvas.redPenguin
+        else:
+            penguin = MyCanvas.penguinImage
+        return self.canvas.create_image(x,y,anchor=tk.NW,image=penguin)
 
-    def drawTrash(self):
-         self.canvas.create_image(0,0,anchor=NW,image=MyCanvas.trashbagImage)
+    def drawTrash(self,x,y):
+        return self.canvas.create_image(x,y,anchor=tk.NW,image=MyCanvas.trashbagImage)
          
-    def deleteTrash(self):
-        pass
-    def deadPenguin(self):
-        pass
+    # def deleteTrash(self):
+    #     pass
+    # def deadPenguin(self):
+    #     pass
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
 import canvas
+import random
 import time
 
 def buildWindow():
@@ -20,29 +21,40 @@ def buildWindow():
     p1.pack(side="left",fill="y",expand=False)
 
     # This is where the user can input a population
-    population = Label(p1, text="Number of Penguins:", background="White")
-    populationEntry = Entry(p1)
+    population = Label(p1, text="Number of Penguins:", background="white",foreground="black")
+    populationentry = Entry(p1)
     population.grid(row = 0, column=0, sticky=W,pady=2)
-    populationEntry.grid(row=0,column=1, sticky=W,pady=2)
+    populationentry.grid(row=0,column=1, sticky=W,pady=2)
     
-    polution = Label(p1, text="Pounds of Polution:", background="White")
-    polutionEntry = Entry(p1)
+    polution = Label(p1, text="Pounds of Polution:", background="white",foreground="black")
+    polutionentry = Entry(p1)
     polution.grid(row = 1, column=0, sticky=W,pady=2)
-    polutionEntry.grid(row=1,column=1, sticky=W,pady=2)
+    polutionentry.grid(row=1,column=1, sticky=W,pady=2)
 
     c1 = canvas.MyCanvas(win)
     c1.canvas.pack(fill="both",expand="true")
 
-    c1width = c1.canvas.winfo_width()
-    c1height = c1.canvas.winfo_height()
-    print(c1width)
-    print(c1height)
-
-
     landscapeImage = Image.open("landscape.png")
-    resized_image3 = landscapeImage.resize((int(landscapeImage.width*5), int(landscapeImage.height*5)), Image.Resampling.LANCZOS)
+    resized_image3 = landscapeImage.resize((int(landscapeImage.width*5), int(landscapeImage.height*5)), Image.ANTIALIAS)
     landscapeImage = ImageTk.PhotoImage(resized_image3)
     c1.canvas.create_image(-50,175,anchor=NW,image=landscapeImage)
+
+    penguinList = []
+    
+    # Have three initial penguins added to the screen (a 4th is added in the update function)
+    penguinList.append(c1.drawPenguin(random.randint(0,750),random.randint(250,600)))
+    penguinList.append(c1.drawPenguin(random.randint(0,750),random.randint(250,600)))
+    penguinList.append(c1.drawPenguin(random.randint(0,750),random.randint(250,600)))
+
+    # Update function for tkinter window
+    def update():
+        nonlocal penguinList
+        penguinList.append(c1.drawPenguin(random.randint(0,750),random.randint(200,600)))
+        if(len(penguinList) > 8):
+            c1.canvas.delete(penguinList[0])
+            del penguinList[0]
+        win.after(random.randint(2,5)*1000,update)
+    update()
     win.mainloop()
 
 
